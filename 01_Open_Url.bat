89d348a03a68dc864cda7da73cf1daa618c385e8
satya <23057045@kiit.ac.in>
2025-07-03 16:23:44 +0530

    commit

--- 01_Open_Url.bat
+++ 01_Open_Url.bat
@@ -6,4 +6,5 @@ start chrome.exe www.youtube.com
 start msedge.exe www.google.com
 start brave.exe www.facebook.com
 start edge.exe www.yandex.com
+start firefox.exe www.codewithharry.com
 
--- 02_Open_Url_Incognito.bat
+++ 02_Open_Url_Incognito.bat
@@ -5,3 +5,4 @@ pause
 start msedge.exe -incognito https://www.youtube.com
 start  /d "C:\Program Files\Google\Chrome\Application" chrome.exe -incognito https://www.yandex.com
 chrome.exe -incognito https://www.facebook.com
+brave.exe -incognito https://www.rahulshettyacademy.com
